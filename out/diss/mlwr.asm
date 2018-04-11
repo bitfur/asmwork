23c9a17b60a6577e33488dc8677f0b194ee0e76e
BitFur <bitfur@users.noreply.github.com>
2018-04-12 01:22:34 +0300

    byte moving around

---
+++ out/diss/mlwr.asm
@@ -0,0 +1,18 @@
+SECTION .data
+msg: db "MALWARE",10,0
+msg_len: equ $-msg
+SECTION .bss
+SECTION .text
+global _start
+_start:
+	nop
+	mov ebx,msg
+	mov eax,7
+process:add byte [ebx],32
+	inc ebx
+	dec eax
+	jnz process
+	nop
+	mov eax,1
+	mov ebx,0
+	int 80H
